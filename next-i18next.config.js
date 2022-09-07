055859bd788c511a4763eef9e8ededac4b0d2234
nastya-nealova <nastya20001492@gmail.com>
2022-09-07 13:53:37 +0300

    small fixes

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ module.exports = {
     // https://www.i18next.com/overview/configuration-options#logging
     // debug: process.env.NODE_ENV === 'development',
     i18n: {
-        locales: ['en', 'en_US', 'uk_UA'],
-        defaultLocale: 'uk_UA',
+        locales: ['en', 'en_US', 'uk'],
+        defaultLocale: 'uk',
     },
     // reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
